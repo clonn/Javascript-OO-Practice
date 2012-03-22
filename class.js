a32205c8b80dd5a94cebcd16b97ce795070bd610
Caesar Chi <caesar_chi@miiicasa.com>
2012-03-22 11:50:57 +0800

    add private variable, and function, sprate the public function in Person Class.

--- class.js
+++ class.js
@@ -1,19 +1,44 @@
 var Person = function (name, age) {
     var name,
         age,
-        toString;
+        year,
+        // constance.
+        FULL_YEAR = 1911,
+        // private function.
+        countYear;
 
-    this.name = name;
-    this.age = age;
+    name = name;
+    age = age;
 
-    this.toString = function () {
-        return '[' + this.name + ',' + this.age + ']';
+    // private function.
+    countYear = function () {
+        year = age + FULL_YEAR;
     };
+
+    // public function.
+    this.toString = function () {
+        return '[' + name + ',' + age + ',' + year + ']';
+    };
+    this.getName = function () {
+        return name;
+    };
+    this.getAge = function () {
+        return age;
+    };
+    this.getYear = function () {
+        return year;
+    }
 };
 
 var p = {};
 Person.call(p, "Jason", 30);
-console.log(p);
+console.log(p.age);
+// undefined.
+console.log(p.getAge());
+// 30
 
 var a = new Person("Janet", 15);
-console.log(a);
+console.log(a.age);
+// undefined.
+console.log(a.getAge());
+// 15
